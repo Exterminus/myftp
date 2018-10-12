471842b60c42a528037649d267c07fed300c9058
exterminus <cmagnobarbosa@gmail.com>
2018-10-11 23:21:24 -0300

    Versao inicial login,servidorSenha, ServidorConexao

--- Cliente.py
+++ Cliente.py
@@ -1,25 +1,48 @@
 # encoding:utf8
 import sys
 import socket
+import getpass
+#utilizado para serialização de dados..
+import pickle
 class Cliente(object):
     """Classe cliente conecta
      ao servido usando tcp"""
     #ip do servidor de conexao
     #self.ip=""
     def __init__(self):
-        #self.ip_conexao=0
-        #self.porta=0
+        self.ip_conexao=0
+        self.porta=0
         self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def valida_dados(self,ip,porta):
+        pass
+
+    def login(self,usuario):
+        """Realiza o pedido de Login"""
+        print("Dados conexão\nIP:",self.ip_conexao,"Porta:",self.porta,"Usuario:",usuario)
+        senha=getpass.getpass("senha: ")
+        #print("IP:",self.ip_conexao,"Porta:",self.porta,"Usuario:",usuario,"senha:",senha)
+        msg={"usuario":usuario,"senha":senha}
+        #dicionario é serializado em bytes..
+        msg_b=pickle.dumps(msg)
+        self.tcp.sendall(msg_b)
+        retorno=self.tcp.recv(1024)
+        #print("Retorno",pickle.loads(retorno))
+        if(pickle.loads(retorno)):
+            return True
+        else:
+            return False
     def inicia_conexao(self,ip,porta):
         """Inicia a conexão com servidor."""
         self.ip_conexao=ip
-        self.porta=porta
+        self.porta = porta
         destino=(self.ip_conexao,int(self.porta))
         self.tcp.connect(destino)
-
+        retorno=self.tcp.recv(1024)
+        print("Mensagem Inicial:",retorno.decode("utf-8"))
     def console(self):
-        print("Digite a sua mensagem")
+        """Inicia o console de comandos"""
+        #print("Digite a sua mensagem   ")
         while(True):
             try:
                 # # Send data
@@ -27,10 +50,10 @@ class Cliente(object):
                 # print('sending {!r}'.format(message))
                 # self.tcp.sendall(message)
                 # Look for the response
-                data = self.tcp.recv(1024)
-                print(repr(data))
-                comando=input()
+                comando=input(">> ")
                 self.tcp.sendall(comando.encode())
+                data = self.tcp.recv(1024)
+                print(data.decode("utf8"))
             except Exception as e:
                 print(e)
                 print('closing socket')
@@ -39,4 +62,8 @@ class Cliente(object):
         tcp.close()
 cliente=Cliente()
 cliente.inicia_conexao(sys.argv[1],sys.argv[2])
-cliente.console()
+senha=cliente.login(sys.argv[3])
+if(senha):
+    cliente.console()
+else:
+    print("Não foi possível efetuar o login.")
--- ServidorConexao.py
+++ ServidorConexao.py
@@ -2,45 +2,79 @@
 import sys
 import socket
 import _thread as thread
+import getpass
+import xmlrpc.client
+import pickle
 class ServidorConexao(object):
     """docstring for ServidorConexao."""
     def __init__(self):
-        self.porta=5003
+        self.porta=int(sys.argv[1])
         self.ip=""
         self.tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp.bind((self.ip,self.porta))
         self.tcp.listen(1)
+        #chamada RPC para o servidor de senha.
+        self.senha_auth=xmlrpc.client.ServerProxy("http://localhost:8000/")
+
+    def rpc_senha(self,usuario,senha):
+        """Valida a senha e usuario"""
+        print(usuario,senha)
+        try:
+            retorno=self.senha_auth.login(usuario,senha)
+            if(retorno):
+                #devolve a resposta de login sucesso
+                return True
+            else:
+                return False
+        except xmlrpc.client.Fault as err:
+            print("Ocorreu um erro")
+            print("Código de erro %d" % err.faultCode)
+            print("Fault string: %s" % err.faultString)
 
     def processa_comando(self,conexao,comando):
+        #encerra a conexao com o cliente
         if(comando.decode("utf-8")=="logout"):
-            conexao.sendall(b"conexao encerrada...")
+            conexao.sendall(b"-1")
             conexao.close()
             thread.exit()
 
+
     def conectado(self,connection, cliente):
-        connection.sendall(b'Bem vindo ao Zeus FTP')
-        while True:
-            # Wait for a connection
-            try:
-                print('connection from', cliente)
-                # Receive the data in small chunks and retransmit it
-                data = connection.recv(1024)
-                print(data)
-                print('received {!r}'.format(data))
-                self.processa_comando(connection,data)
-                if data:
-                    print('sending data back to the client')
-                    connection.sendall(data)
-                else:
-                    print('no data from', cliente)
-                    break
-            except Exception as e:
-                # Clean up the connection
-                print("e",e)
-                connection.close()
-                thread.exit()
+        """verificar o disparo e encerramento de threads"""
+        connection.sendall(b'Bem vindo ao Zeus FTP v 1.0 2018.')
+        estado_login=False
+        data=connection.recv(1024)
+        dados_login=pickle.loads(data)
+        if(self.rpc_senha(dados_login['usuario'],dados_login['senha'])):
+            estado_login=True
+            #sucesso ao realizar o login
+            connection.sendall(pickle.dumps(True))
+        else:
+            #erro ao efetuar o login
+            connection.sendall(b"False")
+        if(estado_login== True):
+            while True:
+                # Wait for a connection
+                try:
+                    print('connection from', cliente)
+                    # Receive the data in small chunks and retransmit it
+                    data = connection.recv(1024)
+                    print(data)
+                    print('received {!r}'.format(data))
+                    self.processa_comando(connection,data)
+                    if data:
+                        print('sending data back to the client')
+                        connection.sendall(data)
+                    else:
+                        print('no data from', cliente)
+                        break
+                except Exception as e:
+                    # Clean up the connection
+                    print("e",e)
+                    connection.close()
+                    thread.exit()
     def iniciar_servidor(self):
-        print("Servidor Iniciado..")
+        print("Servidor Iniciado.. na porta",self.porta)
         while True:
             con, cliente = self.tcp.accept()
             thread.start_new_thread(self.conectado, tuple([con, cliente]))
